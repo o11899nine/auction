9386b7b68f3edbdefd0cd3a313659649be79a622
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-04-13 14:15:21 +0200

    new_page option added

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
     path("search", views.search, name="search"),
-    path("random", views.random_page, name="random_page")
+    path("random", views.random_page, name="random_page"),
+    path("new_page", views.new_page, name="new_page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django import forms
 from markdown2 import markdown
+from django.contrib import messages
 
 from . import util
 
@@ -14,12 +15,21 @@ class SearchForm(forms.Form):
         widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'})
     )
 
+class EntryForm(forms.Form):
+    title = forms.CharField(
+        label="",
+        widget=forms.TextInput(attrs={'placeholder': 'Enter title'})
+    )
+    contents = forms.CharField(
+        label="",
+        widget=forms.Textarea(attrs={"placeholder": "Enter contents", "style": "resize:none;height:400px;"})
+    )
 
 def index(request):
     """ Shows list of all entries. Clicking on an entry redirects to that entry's page"""
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        "form": SearchForm()
+        "search_form": SearchForm()
     })
 
 
@@ -35,7 +45,7 @@ def entry(request, entry):
     if content == None:
         return render(request, "encyclopedia/entry_error.html", {
             "entry": entry,
-            "form": SearchForm()
+            "search_form": SearchForm()
         })
 
     # If the entry does exist, the user should be presented with a page that displays the content of the entry.
@@ -43,7 +53,7 @@ def entry(request, entry):
         return render(request, "encyclopedia/entry.html", {
             "entry": entry,
             "content": markdown(content),
-            "form": SearchForm()
+            "search_form": SearchForm()
         })
 
 
@@ -78,7 +88,7 @@ def search(request):
                 return render(request, "encyclopedia/search.html", {
                     "query": query,
                     "search_results": search_results,
-                    "form": SearchForm()
+                    "search_form": SearchForm()
                 })
 
         return redirect("encyclopedia:index")
@@ -89,3 +99,36 @@ def search(request):
 def random_page(request):
     """Clicking “Random Page” in the sidebar should take user to a random encyclopedia entry."""
     return redirect("encyclopedia:entry", entry=random.choice(util.list_entries()))
+
+
+def new_page(request):
+    """Clicking “Create New Page” in the sidebar should take the user to a page where they can create a new encyclopedia entry."""
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the title and contents from the 'cleaned' version of form data
+            title = form.cleaned_data["title"]
+            contents = form.cleaned_data["contents"]
+
+            # If an encyclopedia entry already exists with the provided title, the user should be presented with an error message.
+            if util.get_entry(title) != None:
+                messages.error(request, "An entry with this title already exists")
+                return redirect("encyclopedia:new_page")
+
+
+            # Otherwise, the encyclopedia entry should be saved to disk, and the user should be taken to the new entry’s page.
+            else:
+               with open(f"entries/{title}.md", "a") as file:
+                file.write(f"#{title}\n")
+                file.write(contents)
+                return redirect("encyclopedia:entry", entry=title)
+
+
+
+    else:
+        return render(request, "encyclopedia/new_page.html", {
+            "entry_form": EntryForm(),
+            "search_form": SearchForm()})
