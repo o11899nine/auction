c4d78edf373d25d7c70951351eea3f9bc478ae28
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-04-15 12:43:35 +0200

    listing model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    CATEGORIES = (
+        ('Toys', 'Toys'),
+        ('Cars', 'Cars'),
+        ('Clothing', 'Clothing'),
+        ('Furniture', 'Furniture'),
+        ('Instruments', 'Instruments'),
+    )
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=5000)
+    starting_bid = models.FloatField()
+    image_url = models.URLField()
+    category = models.CharField(max_length=1, choices=CATEGORIES)
