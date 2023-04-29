3d5d413bbb5ed94fb8dc8d9fa17300a0d601ca67
Marcel Waterlander <112943380+o11899nine@users.noreply.github.com>
2023-04-29 14:45:34 +0200

    added comment system

--- auctions/globals.py
+++ auctions/globals.py
@@ -8,3 +8,7 @@ CATEGORIES = (
 )
 
 PLACEHOLDER_IMG = "https://lh3.googleusercontent.com/EbXw8rOdYxOGdXEFjgNP8lh-YAuUxwhOAe2jhrz3sgqvPeMac6a6tHvT35V6YMbyNvkZL4R_a2hcYBrtfUhLvhf-N2X3OB9cvH4uMw=w1064-v0"
+
+POST_ICON = '<i class="material-icons" style="font-size:1rem;color:white">send</i>'
+BID_ICON = '<i class="material-icons" style="font-size:1rem;color:white">gavel</i>'
+DELETE_ICON = '<i class="material-icons" style="font-size:1rem;color:red;opacity:0.5;">delete</i>'
